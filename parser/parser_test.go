8d3fcbe4488186c22fa7c3d811ca39f9b7a2d6dd
tomocy <tomocy.dev@gmail.com>
2019-01-07 16:25:15 +0900

    refactor parser test to test expression depending on the type

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -5,22 +5,42 @@ import (
 
 	"github.com/tomocy/kinako/ast"
 	"github.com/tomocy/kinako/lexer"
+	"github.com/tomocy/kinako/token"
 )
 
 func TestParseExpression(t *testing.T) {
-	input := `
-	5
-	`
-	expect := &ast.Integer{
-		Value: 5,
+	tests := []struct {
+		input    string
+		expected ast.Expression
+	}{
+		{
+			"5",
+			&ast.Integer{
+				Token: token.Token{
+					Type:    token.Integer,
+					Literal: "5",
+				},
+				Value: 5,
+			},
+		},
 	}
-	parser := New(lexer.New(input))
-	expr := parser.parseExpression()
-	integer, ok := expr.(*ast.Integer)
-	if !ok {
-		t.Errorf("unexpected expression: got %T, expected *ast.Integer\n", expr)
+	for _, test := range tests {
+		parser := New(lexer.New(test.input))
+		expr := parser.parseExpression()
+		switch expr := expr.(type) {
+		case *ast.Integer:
+			testParseIntegerExpression(t, expr, test.expected.(*ast.Integer))
+		default:
+			t.Fatalf("unexpected type of expression: %T\n", expr)
+		}
 	}
-	if integer.Value != expect.Value {
-		t.Errorf("unexpected value: got %d, expected %d\n", integer.Value, expect.Value)
+}
+
+func testParseIntegerExpression(t *testing.T, actual, expected *ast.Integer) {
+	if actual.Token != expected.Token {
+		t.Errorf("unexpected token: got %v, but expected %v\n", actual.Token, expected.Token)
+	}
+	if actual.Value != expected.Value {
+		t.Errorf("unexpected value: got %d, but expected %d\n", actual.Value, expected.Value)
 	}
 }
