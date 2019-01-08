eb0f21162ab301a56fb59756470f1a93787c7352
tomocy <tomocy.dev@gmail.com>
2019-01-08 16:52:28 +0900

    fix to make Expression implement Node

--- ast/ast.go
+++ ast/ast.go
@@ -5,16 +5,25 @@ import (
 )
 
 type Node interface {
-	Expression
+	node()
+}
+
+type Statement interface {
+	Node
+	statement()
 }
 
 type Expression interface {
+	Node
 	expression()
 }
 
 type Integer struct {
 	Token token.Token
 	Value int64
+}
+
+func (i Integer) node() {
 }
 
 func (i Integer) expression() {
