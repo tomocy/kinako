ba226b9888dbb0925e15cc2b8964ef5761196960
tomocy <tomocy.dev@gmail.com>
2019-01-11 20:23:32 +0900

    add repl test to check variable declaration

--- repl/repl_test.go
+++ repl/repl_test.go
@@ -14,6 +14,8 @@ func TestPrintResult(t *testing.T) {
 		{"-6;", "-6\n"},
 		{"7 + 8 - 9 * 10 / 11;", "15\n"},
 		{"(12 + 13) / 14;", "1\n"},
+		{"var x int = 15;", "15\n"},
+		{"var x int = 16; x;", "16\n"},
 		{"15; 16", "failed to find semicolon\n"},
 		{"5 / 0;", "divided by zero\n"},
 	}
@@ -25,7 +27,7 @@ func TestPrintResult(t *testing.T) {
 		mock.printResult(test.input)
 		actual := w.String()
 		if actual != test.expected {
-			t.Errorf("unexpected result: got %s, but expected %s\n", actual, test.expected)
+			t.Errorf("unexpected result: got %s, but expected %s\n", actual, test.expected) // TODO: use %q as formatter
 		}
 	}
 }
