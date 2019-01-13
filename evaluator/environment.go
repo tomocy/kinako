d11f2f0eb9e6c28ea20dcfd602222163dcef651c
tomocy <tomocy.dev@gmail.com>
2019-01-13 21:18:34 +0900

    add bultin variable in environment

--- evaluator/environment.go
+++ evaluator/environment.go
@@ -4,13 +4,15 @@ import "github.com/tomocy/kinako/object"
 
 type Environment map[string]object.Object
 
+var builtins = Environment{
+	"true": &object.Boolean{
+		Value: true,
+	},
+	"false": &object.Boolean{
+		Value: false,
+	},
+}
+
 func NewEnvironment() Environment {
-	return Environment{
-		"true": &object.Boolean{
-			Value: true,
-		},
-		"false": &object.Boolean{
-			Value: false,
-		},
-	}
+	return builtins
 }
